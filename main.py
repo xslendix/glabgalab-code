83c3824cb65fa43f1cfd8d30a255195fb34a55a3
xslendix <gamingxslendix@gmail.com>
2019-05-17 20:56:40 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+
+debug = True
+
+import basic
+import sys
+import os.path
+import readline
+import optparse
+
+parser = optparse.OptionParser()
+
+parser.add_option('-f', '--file',
+                action='store', dest='filename',
+                help='File name', default=None
+                )
+
+options, args = parser.parse_args()
+
+# if options.filename != None:
+#     if os.path.isfile(options.filename):
+#         with open(options.filename) as f:
+#             lines = f.readlines()
+#             for ln in lines:
+#                 result, error = basic.run(options.filename, ln.replace('\n', ''))
+#                 if error:
+#                     print(error.as_string())
+#                 elif result:
+#                     print(result)
+# else:
+motd = (
+'Glabgalab interpreter 0.0.1\n'
+'---------------------------')
+
+print(motd)
+
+while True:
+    try:
+        text = input('>>> ')
+
+        if text != 'exit':
+            result, error = basic.run('<stdin>', text)
+            if error:
+                print(error.as_string())
+            elif result:
+                print(result)
+        else: break
+    except:
+        if debug == False: print('\nIf you try to exit, use the `exit` command god dammit')
+        else:
+            print('')
+            raise
