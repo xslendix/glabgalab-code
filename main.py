de24533b7ec72a8e006f309cbe5b72ba68513d9f
xslendix <gamingxslendix@gmail.com>
2019-07-30 09:00:26 +0300

    idk

--- main.py
+++ main.py
@@ -18,42 +18,40 @@ parser.add_option('-f', '--file',
 
 options, args = parser.parse_args()
 
-# if options.filename != None:
-#     if os.path.isfile(options.filename):
-#         with open(options.filename) as f:
-#             lines = f.readlines()
-#             for ln in lines:
-#                 result, error = basic.run(options.filename, ln.replace('\n', ''))
-#                 if error:
-#                     print(error.as_string())
-#                 elif result:
-#                     print(result)
-# else:
-motd = (
-'Glabgalab interpreter 0.0.1\n'
-'---------------------------')
-
-print(motd)
-
-while True:
-    try:
-        text = input('>>> ')
-
-        if text.strip() == '': continue            
-
-        if text != 'exit':
-            #result, error = basic.run('<stdin>', text)
-            result, error = run.run('<stdin>', text)
+if options.filename != None:
+    if os.path.isfile(options.filename):
+        with open(options.filename) as f:
+            result, error = run.run(options.filename, f.read())
             if error:
                 print(error.as_string())
             elif result:
-                if len(result.elements) == 1:
-                    print(repr(result.elements[0]))
-                else:
-                    print(repr(result))
-        else: break
-    except:
-        if debug == False: print('\nIf you try to exit, use the `exit` command god dammit')
-        else:
-            print('')
-            raise
+                print(result)
+else:
+    motd = (
+    'Glabgalab interpreter 0.0.1\n'
+    '---------------------------')
+    
+    print(motd)
+
+    while True:
+        try:
+            text = input('>>> ')
+
+            if text.strip() == '': continue            
+
+            if text != 'exit':
+                #result, error = basic.run('<stdin>', text)
+                result, error = run.run('<stdin>', text)
+                if error:
+                    print(error.as_string())
+                elif result:
+                    if len(result.elements) == 1:
+                       print(repr(result.elements[0]))
+                    else:
+                        print(repr(result))
+            else: break
+        except:
+            if debug == False: print('\nIf you try to exit, use the `exit` command god dammit')
+            else:
+                print('')
+                raise
